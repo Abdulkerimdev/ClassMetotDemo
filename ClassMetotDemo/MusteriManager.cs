f7c9f183b9aaf748f688f08928aece870b12bb7f
abdulkerim dev <abdulkerimdev@windowslive.com>
2021-01-31 16:15:43 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriList = new List<Musteri> { };
+        public void Ekle(Musteri musteri)
+        {
+            if (musteri.Referans>0)
+            {
+                Console.WriteLine("Sisteme eklendi " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Referans + " adet referans ile");
+                musteriList.Add(musteri);
+            }
+            else
+            {
+                Console.WriteLine("Üzgünüz.. Mutlaka bir referansınız olmalı.");
+            }
+        }
+        public void Listele()
+        {
+            Console.WriteLine("*******************\nMüşteri listesi");
+            foreach (var musteri in musteriList)
+            {
+                Console.WriteLine("\nAd: " + musteri.Ad + "\nSoyad:" + musteri.Soyad +
+                    "\nGüven Puanı: " + musteri.Guven + "\nReferans Sayısı: " + musteri.Referans + "\n*********************");
+            }
+        }
+        public void Guven(Musteri musteri)
+        {
+           musteri.Guven += 1;
+           Console.WriteLine(musteri.Ad + " " + musteri.Soyad + "kişisine 1 güven puanı verildi. Güncel güven puanı: " + musteri.Guven);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            musteriList.Remove(musteri);
+            Console.WriteLine(musteri.Ad + musteri.Soyad + "Yönetimin kararıyla başarıyla silinmiştir.");
+        }
+
+        public void Kontrol(Musteri musteri)
+        {
+            if (musteri.Guven < -4)
+            {
+                musteriList.Remove(musteri);
+                Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " Güven puanı düşüklüğü sebebiyle kaydı silinmiştir.");
+            }
+            else
+            {
+                Console.WriteLine("Müşterinin güven puanı gereken düzeydedir.Puanı: "+ musteri.Guven + "\nİyi günler \n");
+            }
+            
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seçkin Bank'a Hoşgeldiniz\n\n");
+
+            Musteri musteri1 = new Musteri() { Ad = "sami", Soyad = "samioğulları", Guven = 4, Id = 1, Referans = 2 };
+            Musteri musteri2 = new Musteri() { Ad = "Mehmet", Soyad = "tosuncuk", Guven = -20, Id = 2, Referans = 1 };
+            Musteri musteri3 = new Musteri() { Ad = "mustafa", Soyad = "yılmaz", Guven = 5, Id = 3, Referans = 1 };
+            Musteri musteri4 = new Musteri() { Ad = "derya", Soyad = "yılmaz", Guven = 0, Id = 4, Referans = 0 };
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri4);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Kontrol(musteri2);
+            musteriManager.Kontrol(musteri3);
+            musteriManager.Guven(musteri3);
+            musteriManager.Listele();
+
+
+        }
+    }
+}
